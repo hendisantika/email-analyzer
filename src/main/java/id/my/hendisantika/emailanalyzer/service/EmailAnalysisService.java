5695118e5ebdc95871d07b5e04c0f670829dc6f0
hendisantika <hendisantika@yahoo.co.id>
2026-02-15 06:42:56 +0700

    feat: add analyze in EmailAnalysisService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/emailanalyzer/service/EmailAnalysisService.java
+++ src/main/java/id/my/hendisantika/emailanalyzer/service/EmailAnalysisService.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.emailanalyzer.service;
 
+import id.my.hendisantika.emailanalyzer.dto.EmailAnalysisResult;
+import id.my.hendisantika.emailanalyzer.entity.EmailAnalysisEntity;
 import id.my.hendisantika.emailanalyzer.repository.EmailAnalysisRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +35,51 @@ public class EmailAnalysisService {
             "bank", 5,
             "confirm", 3
     );
+
+    public EmailAnalysisService(EmailAnalysisRepository repo) {
+        this.repo = repo;
+    }
+
+    public EmailAnalysisResult analyze(String emailContent, String username) {
+        // score calculation as before
+        int score = 0;
+        List<String> foundKeywords = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> entry : SUSPICIOUS_KEYWORDS.entrySet()) {
+            String content = emailContent.toLowerCase();
+
+            if (content.contains(entry.getKey())) {
+                // if (emailContent.contains(entry.getKey())) {
+                score += entry.getValue();
+                foundKeywords.add(entry.getKey());
+            }
+        }
+
+        // Détection des liens
+        Pattern linkPattern = Pattern.compile("(https?:\\/\\/\\S+)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = linkPattern.matcher(emailContent);
+
+        while (matcher.find()) {
+            score += 4;
+            foundKeywords.add("Lien suspect : " + matcher.group());
+        }
+
+
+        boolean suspicious = score >= 5;
+        String message = suspicious ? "⚠️ Suspicious email detected" : "Secure email";
+        EmailAnalysisResult result = new EmailAnalysisResult(message, suspicious, score, foundKeywords);
+
+
+        EmailAnalysisEntity entity = new EmailAnalysisEntity();
+        entity.setUsername(username);
+        entity.setContent(emailContent);
+        entity.setSuspicious(result.isSuspicious());
+        entity.setScore(result.getScore());
+        entity.setReasons(result.getReasons());
+
+        repo.save(entity);
+
+        return result;
+    }
 
 }
