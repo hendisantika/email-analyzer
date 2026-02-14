30c7e14b453c56752e1f253663678eb261b1af9f
hendisantika <hendisantika@yahoo.co.id>
2026-02-15 06:37:51 +0700

    feat: add UserEntity🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/emailanalyzer/model/UserEntity.java
@@ -0,0 +1,65 @@
+package id.my.hendisantika.emailanalyzer.model;
+
+import jakarta.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : email-analyzer
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/02/26
+ * Time: 06.37
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    enum Role { USER, ADMIN }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+
+    // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
