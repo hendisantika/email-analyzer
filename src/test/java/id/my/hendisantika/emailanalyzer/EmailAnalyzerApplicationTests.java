0ea88a7734df2b1ed58410e7cc29f482e028d0f0
hendisantika <hendisantika@yahoo.co.id>
2026-02-15 06:28:54 +0700

    feat: init project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/emailanalyzer/EmailAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.emailanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
