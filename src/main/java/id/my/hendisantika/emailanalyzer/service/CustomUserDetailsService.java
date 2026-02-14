34b2580a60de9a09eb1b1df51d6ae99bea81f263
hendisantika <hendisantika@yahoo.co.id>
2026-02-15 06:39:26 +0700

    feat: add CustomUserDetailsService🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/emailanalyzer/service/CustomUserDetailsService.java
@@ -0,0 +1,44 @@
+package id.my.hendisantika.emailanalyzer.service;
+
+import id.my.hendisantika.emailanalyzer.model.UserEntity;
+import id.my.hendisantika.emailanalyzer.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : email-analyzer
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/02/26
+ * Time: 06.39
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRole())
+                .build();
+    }
+}
